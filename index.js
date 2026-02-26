4ce79a3253a33692d049cf07921df4238eaf9352
Divyanshu Shah <148536322+Divyanshu5009@users.noreply.github.com>
2026-02-26 12:18:28 +0530

    update

--- index.js
+++ index.js
@@ -1,16 +1,56 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
 const app = express();
-app.listen(8080);
-app.set('view engine', 'ejs');
-const products = [
-    {id: 1, name: 'Product 1', price: 10},
-    {id: 2, name: 'Product 2', price: 20},
-    {id: 3, name: 'Product 3', price: 30},
-];
-app.get('/', (req, res) => {
-//    res.json({message: 'Hello World'});
-    res.render('index');
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+const dbConnect = async () => {
+  await mongoose.connect("mongodb://localhost:27017/merndatabase");
+};
+const startServer = async () => {
+  await dbConnect();
+  app.listen(8080, () => console.log("Server started"));
+};
+const productSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  imageurl: { type: String, required: true },
 });
-app.get('/products', (req, res) => {
-    res.render('products', { products });
+const productModel = mongoose.model("products", productSchema);
+app.get("/", async (req, res) => {
+  const products = await productModel.find();
+  // res.json(products);
+  res.render("index", { products });
 });
+
+app.get("/add", (req, res) => {
+  res.render("add");
+});
+app.post("/save", async (req, res) => {
+  const body = req.body;
+  const result = await productModel.create(body);
+  res.redirect("/");
+  // res.json({ message: "Product created" });
+});
+
+app.get("/:id/edit", async (req, res) => {
+  const id = req.params.id;
+  const product = await productModel.findOne({ _id: id });
+  res.render("edit", { product });
+});
+
+app.post("/:id/save-product", async (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  await productModel.findByIdAndUpdate(id, body);
+  res.redirect("/");
+});
+
+app.get("/:id/delete", async (req, res) => {
+  const id = req.params.id;
+  await productModel.findByIdAndDelete(id);
+  res.redirect("/");
+});
+
+startServer();
